8c39202bc9b2da2b5ac3f62a93dbc41a6c382e25
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-05-31 11:51:16 -0700

    In Progress

--- app/controllers/game_blogs_controller.rb
+++ app/controllers/game_blogs_controller.rb
@@ -2,21 +2,26 @@ class GameBlogsController < ApplicationController
 
   # GET: /game_blogs
   get "/game_blogs" do
+    @gameblogs = GameBlog.all
     erb :"/game_blogs/index.html"
   end
 
   # GET: /game_blogs/new
   get "/game_blogs/new" do
+
     erb :"/game_blogs/new.html"
   end
 
   # POST: /game_blogs
   post "/game_blogs" do
+    @game_blog = GameBlog.new(params)
+    @game_blog.save
     redirect "/game_blogs"
   end
 
   # GET: /game_blogs/5
   get "/game_blogs/:id" do
+    @game_blog = GameBlog.find_by_id(params[:id])
     erb :"/game_blogs/show.html"
   end
 
